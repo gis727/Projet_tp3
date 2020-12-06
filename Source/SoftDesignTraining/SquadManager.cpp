5fc56667af6c0358a63fd312b5734b5591016d28
Ismael Gbian <ismael.gbian@gmail.com>
2020-12-06 01:05:39 -0500

    feat: Updated squad manager. Pawns now choose the closest available attack position

--- Source/SoftDesignTraining/SquadManager.cpp
+++ Source/SoftDesignTraining/SquadManager.cpp
@@ -6,26 +6,43 @@
 
 SquadManager::SquadManager()
 {
-	UE_LOG(LogTemp, Warning, TEXT("SquadManager created!")); // debug
+	// Fill the positions angles
+	const float angle = (2.f * PI) / (float)m_targetPoints;
+	float currentAngle = 0.f;
+
+	for (int i = 0; i < m_targetPoints; i++)
+	{
+		m_targetPositionsAngles.Add(currentAngle);
+		currentAngle += angle;
+	}
 }
 
 SquadManager::~SquadManager()
 {
-	UE_LOG(LogTemp, Warning, TEXT("SquadManager destroyed!")); // debug
 }
 
+/*
+ * Ajoute un membre
+ */
 void SquadManager::AddPawn(ASDTAIController* controller)
 {
 	m_squadMembers.AddUnique(controller);
-	ComputeTargetPositions();
+	Distribute();
 }
 
+/*
+ * Retire un membre
+ */
 void SquadManager::RemovePawn(ASDTAIController* controller)
 {
 	m_squadMembers.Remove(controller);
 	controller->ResetSquadState();
 }
 
+/*
+ * Met à jour le vision de l'escouade
+ * Si plus aucun membre ne voit le joueur, l'escouade est dissoute
+ */
 void SquadManager::UpdateSightLoss()
 {
 	// Check if all players lost sight
@@ -40,61 +57,112 @@ void SquadManager::UpdateSightLoss()
 		// dismantle squad
 		for (auto member : m_squadMembers)
 		{
-			if (!member->AtJumpSegment)
-			{
-				member->m_PlayerInteractionBehavior = ASDTAIController::PlayerInteractionBehavior_Collect;
-				member->AIStateInterrupted();
-			}
+			member->m_PlayerInteractionBehavior = ASDTAIController::PlayerInteractionBehavior_Collect;
+			if (!member->AtJumpSegment) member->AIStateInterrupted();
 		}
 		m_squadMembers.Empty();
 	}
 }
 
+/*
+ * Indique si le pawn d'un controller est un membre
+ */
 bool SquadManager::IsMember(ASDTAIController* controller)
 {
 	return m_squadMembers.Contains(controller);
 }
 
-void SquadManager::ComputeTargetPositions()
+/*
+ * Pour un membre donné, trouve la première position libre la plus proche
+ */
+float SquadManager::GetBestTargetIndex(ASDTAIController* member)
+{
+	// Check if all squad positions are taken
+	bool allPositionsAreTaken = true;
+	for (bool targetPosition : m_targetPositionsState)
+	{
+		allPositionsAreTaken = allPositionsAreTaken && targetPosition;
+	}
+
+	// Get the closest available position
+	float closestPositionIndex = -1;
+	float distToClosestPosition = MAX_FLT;
+
+	for (int posIndex = 0; posIndex < m_targetPositions.Num(); posIndex++)
+	{
+		const FVector targetPosition = m_targetPositions[posIndex];
+		const bool positionTaken = m_targetPositionsState[posIndex];
+
+		const float distToPosition = (targetPosition - member->GetPawn()->GetActorLocation()).Size2D();
+		const bool positionCanBeConsidered = allPositionsAreTaken || !positionTaken;
+
+		if (distToPosition < distToClosestPosition && positionCanBeConsidered)
+		{
+			distToClosestPosition = distToPosition;
+			closestPositionIndex = posIndex;
+		}
+	}
+
+	return closestPositionIndex;
+}
+
+/*
+ * Efface les positions précédemment trouvées
+ */
+void SquadManager::Reset()
+{
+	m_targetPositions.Empty();
+	m_targetPositionsState.Empty();
+
+	for (float angle : m_targetPositionsAngles)
+	{
+		m_targetPositionsState.Add(false);
+	}
+}
+
+/*
+ * Trouve les positions à viser en fonction de la position du joueur
+ */
+void SquadManager::ComputeTargetPositions(ACharacter* player)
+{
+	// Compute the postions around the player
+	for (float angle : m_targetPositionsAngles)
+	{
+		FVector nextTarget = player->GetActorLocation();
+		nextTarget.X += m_maxDistToPlayer * cos(angle);
+		nextTarget.Y += m_maxDistToPlayer * sin(angle);
+
+		m_targetPositions.Add(nextTarget);
+	}
+}
+
+/*
+ * Distribue les positions aux membres de l'escouade
+ */
+void SquadManager::Distribute()
 {
 	if (m_squadMembers.Num() < 2) return;
 
 	ACharacter* player = UGameplayStatics::GetPlayerCharacter(m_squadMembers[0]->GetWorld(), 0);
 	if (!player) return;
 
-	m_targetPositions.Empty();
+	Reset();
+	ComputeTargetPositions(player);
 
-	const int targetPoints = 4;
-	const float angle = (2.f * PI) / (float) targetPoints;
-	const float maxDistToPlayer = 200.f;
-	float currentAngle = 0.f;
-
-	for (int i = 0; i < targetPoints; i++)
-	{
-		FVector nextTarget = player->GetActorLocation();
-		nextTarget.X += maxDistToPlayer * cos(currentAngle);
-		nextTarget.Y += maxDistToPlayer * sin(currentAngle);
-
-		m_targetPositions.Add(nextTarget);
-
-		currentAngle += angle;
-	}
-
-	int currentTarget = 0;
 	for (auto member : m_squadMembers)
 	{
-		// check if the pawn should be updated (not too close)
-		const FVector toPlayer = player->GetActorLocation() - member->GetPawn()->GetActorLocation();
-		const float distToPlayer = toPlayer.Size2D();
+		// check if the member should be updated (not too close)
+		const float distToPlayer = (player->GetActorLocation() - member->GetPawn()->GetActorLocation()).Size2D();
 
-		if (distToPlayer > maxDistToPlayer && !member->AtJumpSegment)
+		if (distToPlayer > m_maxDistToPlayer && !member->AtJumpSegment)
 		{
-			// update the pawn
-			member->UpdateSquadState(m_targetPositions[currentTarget]);
+			// update the member
+			const float bestPositionIndex = GetBestTargetIndex(member);
+			if (bestPositionIndex >= 0)
+			{
+				member->UpdateSquadState(m_targetPositions[bestPositionIndex]);
+				m_targetPositionsState[bestPositionIndex] = true; // let the other members know that this position is taken
+			}
 		}
-
-		// set the next target
-		currentTarget++;
-		currentTarget = currentTarget < targetPoints ? currentTarget : 0;
 	}
 }
--- Source/SoftDesignTraining/SquadManager.h
+++ Source/SoftDesignTraining/SquadManager.h
@@ -18,10 +18,18 @@ public:
 	void RemovePawn(ASDTAIController* controller);
 	bool IsMember(ASDTAIController* controller);
 	void UpdateSightLoss();
-
-	void ComputeTargetPositions();
+	float GetBestTargetIndex(ASDTAIController* member);
+	void ComputeTargetPositions(ACharacter* player);
+	void Distribute();
+	void Reset();
 
 private:
+
+	const float m_maxDistToPlayer = 350.0f; // Rayon du cercle autour du joueur
+	const int m_targetPoints = 4; // Nombre de positions sur le cercle
+
 	TArray<ASDTAIController*> m_squadMembers;
 	TArray<FVector> m_targetPositions;
+	TArray<bool> m_targetPositionsState; // Indique si une position donnée est déja prise par un membre
+	TArray<float> m_targetPositionsAngles; // Angles des positions autour du joueur
 };
